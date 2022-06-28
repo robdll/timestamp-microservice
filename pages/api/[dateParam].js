07703af453e2b734e1f62efcfa05bb6aae464d87
robdll <dilillo.roberto@gmail.com>
2022-06-28 01:27:12 +0100

    fix on date formatted

--- pages/api/[dateParam].js
+++ pages/api/[dateParam].js
@@ -1,13 +1,13 @@
 export default function handler(req, res) {
   let { dateParam } = req.query;
   const val = Number.parseInt(dateParam);
-  const isTimestamp = !isNaN(val) && val >= -8.64e12 && val <= +8.64e12;
+  const isTimestamp = !isNaN(val) && /\d{5,}/.test(dateParam);
   const isValidDate = new Date(dateParam).getTime() > 0;
   if (!isTimestamp && !isValidDate) {
     res.status(422).json({ error: "Invalid date" });
   } else {
     const date = isTimestamp ? new Date(val) : new Date(dateParam);
-    console.log(dateParam);
+    console.log(date, isTimestamp);
     const data = {
       unix: date.getTime(),
       utc: date.toUTCString(),
